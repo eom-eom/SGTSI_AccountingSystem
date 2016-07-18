3a55b6fbc8d9d7584a29b3320bea0f11de23cf31
DianneMae <dmaesullano@gmail.com>
2016-07-18 14:29:02 +0800

    Adminlte + header

---
+++ templates/head.php
@@ -0,0 +1,14 @@
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title><?php echo $pagetitle?></title>
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		<link href="css/stylesheet.css">
+		<link rel="stylesheet" href="AdminLTE-2.3.5/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+		<link rel="stylesheet" href="AdminLTE-2.3.5/dist/css/AdminLTE.min.css">
+		<link rel="stylesheet" href="AdminLTE-2.3.5/dist/css/skins/_all-skins.min.css">
+	</head>
