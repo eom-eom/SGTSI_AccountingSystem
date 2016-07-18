b2fd00156194933d5251295040606672be381aab
DianneMae <dmaesullano@gmail.com>
2016-07-18 15:29:34 +0800

    dash

---
+++ dash.php
@@ -0,0 +1,128 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Accounting System</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link href="css/stylesheet.css">
+  <link rel="stylesheet" href="AdminLTE-2.3.5/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="AdminLTE-2.3.5/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="AdminLTE-2.3.5\dist\css\skins\_all-skins.css">
+  
+
+  
+  
+  </head>
+  
+<body class="skin-blue sidebar-mini sidebar-collapse">
+	
+
+<header class="main-header" >
+   <a href="dash.php" class="logo">
+	<span class="logo-lg">
+		Accounting System
+	</span>
+	<span class="logo-lg-mini">
+		AS
+	</span>
+   </a>
+  <nav class="navbar navbar-static-top">
+	<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+      </a>
+	  
+	  
+
+	<div class="navbar-custom-menu">
+		<ul class="nav navbar-nav">
+		<li><a href="#">Users</a></li>
+		<li><a href="#">Log-out</a></li>
+		</ul>
+
+		
+	</div>
+	  
+		
+  </nav>
+</header>
+
+<div class="main-sidebar">
+  <!-- Inner sidebar -->
+  <div class="sidebar">
+  
+  <ul class="sidebar-menu">
+      <li class="header">Accounting Tools</li>
+      <!-- Optionally, you can add icons to the links -->
+		<li>
+			<a href="#">
+				<i class="fa fa-file-text"></i> 
+				<span>General Journal</span>
+			</a>
+		</li>
+		<li class="treeview">
+			<a href="#">
+				<i class="fa fa-file-text"></i> 
+				<span>Ledger</span>
+			</a>
+			<ul class="treeview-menu menu-open">
+					<li><a href="#">
+						<i class="fa fa-file-text-o"></i>
+						<span>General Ledger</span>
+						</a>
+					</li>
+					<li><a href="#">
+						<i class="fa fa-file-text-o"></i>
+						<span>Trial Balance</span>
+						</a>
+					</li>
+					<li><a href="#">
+						<i class="fa fa-file-text-o"></i>
+						<span>Archived Ledger</span>
+						</a>
+					</li>
+			</ul>
+      <li class="treeview">
+			<a href="#">
+				<i class="fa fa-file-text"></i>
+				<span>Financial Statement</span>
+			</a>
+			<ul class="treeview-menu menu-open">
+					<li><a href="#">
+						<i class="fa fa-file-text-o"></i>
+						<span>Balance Sheet</span>
+						</a>
+					</li>
+					<li><a href="#">
+						<i class="fa fa-file-text-o"></i>
+						<span>Income Statement</span>
+						</a>
+					</li>
+					<li><a href="#">
+						<i class="fa fa-file-text-o"></i>
+						<span>Cash Flow</span>
+						</a>
+					</li>
+			</ul>
+      </li>
+    </ul><!-- /.sidebar-menu -->
+  </div>
+</div><!-- /.main-sidebar -->
+
+
+
+<script src="AdminLTE-2.3.5/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>	
+<!-- Bootstrap 3.3.6 -->
+<script src="AdminLTE-2.3.5/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="AdminLTE-2.3.5/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="AdminLTE-2.3.5/dist/js/app.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?libraries=places,drawing&amp;sensor=false&amp;key=AIzaSyB1hBlgm-B5aaLB2470dyJVGyC-nx_UnQg"></script>
+<script src="js/map.js"></script>
+<script src="js/gmaps.js"></script>
+
+</body>
+</html>
