784011a61c38b5ab10faa6b07106bab8b09fd91e
jeseeelle <jesellemalunes@gmail.com>
2016-07-21 17:47:31 +0800

    user
    naiwan :3

--- users.php
+++ users.php
@@ -1,9 +1,17 @@
 <?php
 	require_once('support/config.php');
-	addHead('General Joural');
+	addHead('Users');
 	addNavBar();
 	addSideBar();
-?>
+
+	$data=$connection->myQuery("SELECT
+ 							user_id,
+ 							full_name,
+ 							username,
+ 							user_type
+                        FROM users
+                        ");
+ ?>
 
 <div class="content-wrapper">
 	<?php
@@ -13,7 +21,7 @@
 	<h2> Users </h2>
 	<div class="box">
 	<div class="box-body">
-	    <button type="submit" class="btn btn-primary" style="float:right;" id="btnadd" name="btnadd" onclick="addUser()"><i class="fa fa-plus"></i> Add User</button>
+	    <button type="submit" class="btn btn-primary" style="float:right;" id="btnadd" name="btnadd" onclick="addUser()"><i class="fa fa-plus"></i> User</button>
 	</div>
 	<div class="box-body">
 		<table id="table_user" class="table table-striped table-bordered table-hover">
@@ -27,6 +35,22 @@
 			</tr>
 		</thead>
 		<tbody>
+            <?php
+                while($row = $data->fetch(PDO::FETCH_ASSOC)):
+            ?>
+            <tr>
+	            <td><?php echo htmlspecialchars($row['user_id'])?></td>
+	            <td><?php echo htmlspecialchars($row['full_name'])?></td>
+	            <td><?php echo htmlspecialchars($row['username'])?></td>
+	            <td><?php echo htmlspecialchars($row['user_type'])?></td>
+	            <td class='text-center'>
+	            <a href='user_update.php?id=<?php echo $row['user_id']; ?>' class='btn btn-success btn-sm'><span class='fa fa-pencil'></span></a>
+	            <a href='user_delete.php?id=<?php echo $row['user_id']; ?>&t=prod' onclick="return confirm('This record will be deleted.')" class='btn btn-danger btn-sm'><span class='fa fa-trash'></span></a>
+	            </td>
+            </tr>
+            <?php
+                endwhile;
+            ?>
 	</div>
 	</div>	
 </section>
